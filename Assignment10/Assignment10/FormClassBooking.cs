ba40a6140da283a690b6e8328c64749098e5c8e8
Levon Swenson <32556596+Levoni@users.noreply.github.com>
2018-05-02 18:04:26 -0500

    Worked on search

--- Assignment10/Assignment10/FormClassBooking.cs
+++ Assignment10/Assignment10/FormClassBooking.cs
@@ -37,7 +37,7 @@ namespace Assignment10
             dataGridView1.EndEdit();
             Oracle.bookingAdapter.Update(Oracle.myTable);
          }
-         catch 
+         catch
          {
 
          }
@@ -51,9 +51,26 @@ namespace Assignment10
          string op = comboBox2.Text;
          string value = textBox1.Text;
 
-         //Oracle.bookingCommand.CommandText = "Select * from booking where " + field + op + "'" + value + "'";
+         //for non date things
+         if (field != "DATE_TO" && field != "DATE_FROM")
+            Oracle.bookingCommand.CommandText = "Select * from booking where " + field + op + "'" + value + "'";
+         else
+         {
+            // date code
+         }
 
-         
+         //testing
+         //MessageBox.Show(Oracle.bookingCommand.CommandText);
+
+         try
+         {
+            Oracle.myTable.Clear();
+            Oracle.bookingAdapter.Fill(Oracle.myTable);
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.Message);
+         }
 
 
       }
@@ -66,16 +83,16 @@ namespace Assignment10
             Oracle.myTable.Clear();
             Oracle.bookingAdapter.Fill(Oracle.myTable);
          }
-         catch(Exception ex)
+         catch (Exception ex)
          {
             MessageBox.Show(ex.Message);
          }
-         
+
       }
 
       private void button4_Click(object sender, EventArgs e)
       {
-
+         Application.Exit();
       }
 
       private void textBox1_TextChanged(object sender, EventArgs e)
