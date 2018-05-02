af4882e8e4d07cf852eb84e9d87b62831ad989a0
Derek Lubecke <derek.lubecke@gmail.com>
2018-05-02 16:09:44 -0500

    Login Form changes

--- Assignment10/Assignment10/FormClassLogin.Designer.cs
+++ Assignment10/Assignment10/FormClassLogin.Designer.cs
@@ -33,6 +33,9 @@
          this.txtBoxPassword = new System.Windows.Forms.TextBox();
          this.btnLogin = new System.Windows.Forms.Button();
          this.btnCancel = new System.Windows.Forms.Button();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
          this.SuspendLayout();
          // 
          // txtBoxUsername
@@ -72,12 +75,42 @@
          // 
          // btnCancel
          // 
+         this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
          this.btnCancel.Location = new System.Drawing.Point(520, 293);
          this.btnCancel.Name = "btnCancel";
          this.btnCancel.Size = new System.Drawing.Size(75, 23);
          this.btnCancel.TabIndex = 4;
          this.btnCancel.Text = "Cancel";
          this.btnCancel.UseVisualStyleBackColor = true;
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(337, 44);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(57, 13);
+         this.label1.TabIndex = 5;
+         this.label1.Text = "UserName";
+         this.label1.Click += new System.EventHandler(this.label1_Click);
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(337, 119);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(53, 13);
+         this.label2.TabIndex = 6;
+         this.label2.Text = "Password";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Location = new System.Drawing.Point(337, 181);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(59, 13);
+         this.label3.TabIndex = 7;
+         this.label3.Text = "Host String";
+         this.label3.Click += new System.EventHandler(this.label3_Click);
          // 
          // FormClassLogin
          // 
@@ -86,13 +119,16 @@
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.CancelButton = this.btnCancel;
          this.ClientSize = new System.Drawing.Size(825, 393);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
          this.Controls.Add(this.btnCancel);
          this.Controls.Add(this.btnLogin);
          this.Controls.Add(this.txtBoxPassword);
          this.Controls.Add(this.txtBoxHost);
          this.Controls.Add(this.txtBoxUsername);
          this.Name = "FormClassLogin";
-         this.Text = "FormClassLogin";
+         this.Text = "Login";
          this.ResumeLayout(false);
          this.PerformLayout();
 
@@ -105,5 +141,8 @@
       private System.Windows.Forms.TextBox txtBoxPassword;
       private System.Windows.Forms.Button btnLogin;
       private System.Windows.Forms.Button btnCancel;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label3;
    }
 }
